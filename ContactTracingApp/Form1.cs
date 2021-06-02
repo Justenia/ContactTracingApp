82b09bd9bf57cf822dc3a8dd64c94ab9c5e92e46
Justenia Maire San Jose <sanjosejusteniamaire@gmail.com>
2021-06-02 14:50:22 +0800

    try code

--- ContactTracingApp/Form1.cs
+++ ContactTracingApp/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,19 @@ namespace ContactTracingApp
 
         private void btn_Save_Click(object sender, EventArgs e)
         {
-            StreamWriter 
+            StreamWriter outputFile;
+            outputFile = File.AppendText("Output.txt");
+
+            outputFile.WriteLine(lbl_lastName.Text + " " + txtBox_lastName.Text);
+            outputFile.WriteLine(lbl_firstName.Text + " " + txtBox_firstName.Text);
+            outputFile.WriteLine(lbl_middleName.Text + " " + txtBox_middleName.Text);
+            outputFile.WriteLine(lbl_age.Text + " " + txtBox_age.Text);
+            outputFile.WriteLine(lbl_gender.Text + " " + comboBox_gender.Text);
+            outputFile.WriteLine(lbl_date.Text + " " + txtBox_date.Text);
+            outputFile.WriteLine(lbl_contactNum.Text + " " + txtBox_contactNum.Text);
+            outputFile.WriteLine(lbl_address.Text + " " + txtBox_address.Text);
+            outputFile.WriteLine(lbl_emailAdd.Text + " " + txtBox_emailAdd.Text);
+            outputFile.Close();
         }
     }
 }
